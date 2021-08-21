6d1a540ac055ab0dc768e84c68f3773c7ab64a2c
Mahsan Ojaghian <77487326+mahsan-ojaghian@users.noreply.github.com>
2021-08-21 16:22:09 +0430

    router-react-dom-library

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./styles.css";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import AllMeetsUpPage from "./pages/AllMeetsUp";
 import NewMeetUpPage from "./pages/NewMeetUp";
 import FavorietsPage from "./pages/Favoriets";
@@ -7,15 +7,17 @@ import FavorietsPage from "./pages/Favoriets";
 export default function App() {
   return (
     <div className="App">
-      <Route path="/">
-        <AllMeetsUpPage />
-      </Route>
-      <Route path="/new-meetup">
-        <NewMeetUpPage />
-      </Route>
-      <Route path="/favoriets">
-        <FavorietsPage />
-      </Route>
+      <Switch>
+        <Route path="/all" exact>
+          <AllMeetsUpPage />
+        </Route>
+        <Route path="/new-meetup">
+          <NewMeetUpPage />
+        </Route>
+        <Route path="/favoriets">
+          <FavorietsPage />
+        </Route>
+      </Switch>
     </div>
   );
 }
