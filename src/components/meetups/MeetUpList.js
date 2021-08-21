d9fed007555788bd322e512bd868bf64ce24b479
Mahsan Ojaghian <77487326+mahsan-ojaghian@users.noreply.github.com>
2021-08-21 20:19:07 +0430

    props

--- src/components/meetups/MeetUpList.js
+++ src/components/meetups/MeetUpList.js
@@ -1,13 +1,16 @@
-const MeetUpItem = () => {
+const MeetUpItem = (props) => {
   return (
     <li>
       <div>
-        <img src={} alt="image" />
+        <img src={props.img} alt="item" />
       </div>
       <div>
-        <h3>last name</h3>
-        <h5>email</h5>
-        <p>gender</p>
+        <h3>{props.last_name}</h3>
+        <h5>{props.email}</h5>
+        <p>{props.gender}</p>
+      </div>
+      <div>
+        <button>to favorite</button>
       </div>
     </li>
   );
