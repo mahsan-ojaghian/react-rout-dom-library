bb445ba6307a4a1b4aceed76fe2310d1e35ecd6a
Mahsan Ojaghian <77487326+mahsan-ojaghian@users.noreply.github.com>
2021-08-21 17:53:43 +0430

    module classes

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { Route, Switch } from "react-router-dom";
 import AllMeetsUpPage from "./pages/AllMeetsUp";
 import NewMeetUpPage from "./pages/NewMeetUp";
 import FavorietsPage from "./pages/Favoriets";
+import MainNavigation from "./components/layouts/MainNavigation";
 
 export default function App() {
   return (
     <div className="App">
+      <MainNavigation />
       <Switch>
         <Route path="/all" exact>
           <AllMeetsUpPage />
---
+++ src/components/layouts/MainNavigation.js
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+import classes from "./MainNavigation.module.css";
+const MainNavigation = () => {
+  return (
+    <header className={classes.header}>
+      <div className={classes.logo}>React MeetUp</div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">some links</Link>
+          </li>
+          <li>
+            <Link to="/new-meetup">some links</Link>
+          </li>
+          <li>
+            <Link to="/favoriets">some links</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+export default MainNavigation;
