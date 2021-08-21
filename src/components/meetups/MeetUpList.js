911c7f4d35a3a852003224dff1ac0820c41392fd
Mahsan Ojaghian <77487326+mahsan-ojaghian@users.noreply.github.com>
2021-08-21 20:13:01 +0430

    item

---
+++ src/components/meetups/MeetUpList.js
@@ -0,0 +1,15 @@
+const MeetUpItem = () => {
+  return (
+    <li>
+      <div>
+        <img src={} alt="image" />
+      </div>
+      <div>
+        <h3>last name</h3>
+        <h5>email</h5>
+        <p>gender</p>
+      </div>
+    </li>
+  );
+};
+export default MeetUpItem;
