67eef1680b9ee0bbdb997ba1fbd6847631568a14
Mahsan Ojaghian <77487326+mahsan-ojaghian@users.noreply.github.com>
2021-08-23 00:06:56 +0430

    today is over

--- src/pages/NewMeetUp.js
+++ src/pages/NewMeetUp.js
@@ -1,5 +1,7 @@
+import { useHistory } from "react-router-dom";
 import NewMeetUpForm from "../components/meetups/NewMeetUpForm";
 function NewMeetUpPage() {
+  const history = useHistory();
   function addMeetupHandler(meetupData) {
     fetch(
       "https://react-getting-started-512ed-default-rtdb.firebaseio.com/meetups.json",
@@ -10,7 +12,9 @@ function NewMeetUpPage() {
           "Content-Type": "application/json"
         }
       }
-    );
+    ).then(() => {
+      history.replace("/");
+    });
   }
   return (
     <section>
