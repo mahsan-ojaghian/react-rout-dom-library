e458bc07a5129e9bd2841f71e82ec9c6b12d719e
Mahsan Ojaghian <77487326+mahsan-ojaghian@users.noreply.github.com>
2021-08-22 14:40:43 +0430

    card component added

--- src/components/meetups/MeetUpItem.js
+++ src/components/meetups/MeetUpItem.js
@@ -1,18 +1,21 @@
 import classes from "./MeetUpItem.module.css";
+import Card from "../Ui/Card";
 const MeetUpItem = (props) => {
   return (
     <li className={classes.item}>
-      <div className={classes.image}>
-        <img src={props.img} alt="item" />
-      </div>
-      <div className={classes.content}>
-        <h3>{props.last_name}</h3>
-        <h5>{props.email}</h5>
-        <p>{props.gender}</p>
-      </div>
-      <div className={classes.actions}>
-        <button>{props.first_name}</button>
-      </div>
+      <Card>
+        <div className={classes.image}>
+          <img src={props.img} alt="item" />
+        </div>
+        <div className={classes.content}>
+          <h3>{props.last_name}</h3>
+          <h5>{props.email}</h5>
+          <p>{props.gender}</p>
+        </div>
+        <div className={classes.actions}>
+          <button>{props.first_name}</button>
+        </div>
+      </Card>
     </li>
   );
 };
